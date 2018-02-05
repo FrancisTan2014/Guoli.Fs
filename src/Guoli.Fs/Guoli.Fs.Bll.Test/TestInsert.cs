72c1f33c830b64e957bd134f7bc75b6c608f3492
FrancisTan <499166435@qq.com>
2018-02-05 09:42:53 +0800

    修复BUG

---
+++ src/Guoli.Fs/Guoli.Fs.Bll.Test/TestInsert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Guoli.Fs.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Guoli.Fs.Bll.Test
+{
+    [TestClass]
+    public class TestInsert
+    {
+        [TestMethod]
+        public void TestAddOperateLog()
+        {
+            var bll = new OperationLogBll();
+            var log = new OperationLog
+            {
+                OperationDesc = "测试",
+                OperationTime = DateTime.Now,
+                OperationType = 1,
+                SystemUserId = 1,
+                TargetTableName = "测试",
+                TargetId = 1
+            };
+
+            bll.Add(log);
+            Assert.AreEqual(true, log.Id > 0);
+        }
+    }
+}
